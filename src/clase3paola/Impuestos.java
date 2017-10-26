8faea9d1ef0de47d80c542f54cabbc57784b5dc8
paolaandyco71 <paolaandyco71@gmail.com>
2017-10-26 20:24:20 -0300

    esta clase calcula impuesto

---
+++ src/clase3paola/Impuestos.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3paola;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author LAB03
+ */
+public class Impuestos
+{ 
+    public static void main(String[]args)
+    {
+        int sueldo;
+        
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("Ingrese su salario: ");
+        sueldo = escaner.nextInt();
+        if (sueldo>600000)
+         
+        {
+           System.out.println("Debe Pegar impuesto");  
+        }
+             
+                
+           
+            
+        
+        
+        
+        
+        
+    }
+            
+            
+}
+
+
+
+
